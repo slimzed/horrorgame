2341d29c2d1fae75ed16586a360192ebcee75380
slimzed <cooldyde21@gmail.com>
2025-07-11 14:56:28 -0700

    simple push, animation on grenade explosion now

--- Assets/Scripts/GrenadeProjectile.cs
+++ Assets/Scripts/GrenadeProjectile.cs
@@ -2,24 +2,44 @@ using UnityEngine;
 
 public class GrenadeProjectile : MonoBehaviour
 {
+    [SerializeField] private Sprite grenadeSprite;
+    
+    
     private Rigidbody2D rb;
     private Collider2D objCollider;
+    private Animator anim;
+    private SpriteRenderer spriteRenderer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         objCollider = GetComponent<Collider2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+
         rb.linearVelocityY = 5f;
         rb.linearVelocityX = 0f;
         objCollider.enabled = false; // temporarily disables upon awaken
+
+        anim.enabled = false;
+
+
         Invoke("EnableCollider", 1);
     }
 
     // Update is called once per frame
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(gameObject);
+        anim.enabled = true;
+        rb.linearVelocity = Vector2.zero; // make sure that the grenaed stands still while the animation is playing.
+        Invoke("AnimDisable", 0.1f);
         // enemyhitbox actually does the checking if its grenade and removes health 
+    }
+    private void AnimDisable()
+    {
+        anim.enabled = false;
+        Destroy(gameObject);
     }
     private void EnableCollider()
     {
